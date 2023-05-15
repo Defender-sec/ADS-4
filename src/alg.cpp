e7d0222b97e829a2265e8103f18161e3a5bcfecc
Misha Filchagin <79020757+Defender-sec@users.noreply.github.com>
2023-05-15 23:38:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int a;
+  for(int i=0; i < len; i++){
+    for(int j=0; j < len; j++){
+      if(i != j){
+        int c = arr[j]+arr[i];
+        if(c == value){
+          a++;
+        }
+      }
+    }
+  }
+  return a;
 }
 int countPairs2(int *arr, int len, int value) {
   return 0;
